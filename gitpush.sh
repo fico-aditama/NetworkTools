4ce9ecf4abf3edaf90ed77605cc246f96a19361e
Fiko Aditama <fiko@simise.id>
2024-05-11 20:41:00 +0700

    Network T001 Collections

---
+++ gitpush.sh
@@ -0,0 +1,6 @@
+git init .
+git add .
+git commit -m "Network T001 Collections"
+git branch -M main
+git remote add origin https://github.com/fico-aditama/NetworkTools.git
+git push -u origin main -f
